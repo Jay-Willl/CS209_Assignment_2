e4cd079f942a53dea90cc3c94d292a673169cbbe
Jay W <977482173@qq.com>
2022-11-22 23:38:17 +0800

    first commit

---
+++ src/ChessboardController.java
@@ -0,0 +1,116 @@
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.*;
+
+public class ChessboardController implements Initializable {
+
+    private static final int PLAY_1 = 1;
+    private static final int PLAY_2 = -1;
+
+    @FXML
+    private Pane background;
+
+    @FXML
+    private Rectangle mainBoard;
+
+    @FXML
+    private TextArea infoTextArea;
+
+    private static final int EMPTY = 0;
+    private static final int BOUND = 90;
+    private static final int OFFSET = 15;
+    private static int[][] chessBoard;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mainBoard.setOnMouseClicked(event -> {
+            if(Objects.equals(Client.getText(), "your turn") || Objects.equals(Client.getText(), "start")){
+                int x = (int) (event.getX() / BOUND);
+                int y = (int) (event.getY() / BOUND);
+                chessBoard = Client.getChessboard();
+                if (chessBoard[x][y] == 0){
+                    Client.setAct(true);
+                    Client.setX(x);
+                    Client.setY(y);
+                } else {
+                    System.out.println("#!  Not a valid place");
+                }
+            }
+        });
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        // set timer to refresh board ad textarea
+                        refreshBoard(Client.getChessboard());
+                        refreshText(Client.getText());
+                    }
+                });
+
+            }
+        },100,500);
+    }
+
+    private void refreshBoard(int[][] chessBoard) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                switch (chessBoard[i][j]) {
+                    case PLAY_1:
+                        drawCircle(i, j);
+                        break;
+                    case PLAY_2:
+                        drawLine(i, j);
+                        break;
+                    case EMPTY:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void refreshText(String content){
+        infoTextArea.clear();
+        infoTextArea.appendText(content);
+    }
+
+    private void drawCircle(int i, int j) {
+        Circle circle = new Circle();
+        background.getChildren().add(circle);
+        circle.setCenterX(i * BOUND + BOUND / 2.0 + OFFSET);
+        circle.setCenterY(j * BOUND + BOUND / 2.0 + OFFSET);
+        circle.setRadius(BOUND / 2.0 - OFFSET / 2.0);
+        circle.setStroke(Color.RED);
+        circle.setFill(Color.TRANSPARENT);
+    }
+
+    private void drawLine(int i, int j) {
+        Line line_a = new Line();
+        Line line_b = new Line();
+        background.getChildren().add(line_a);
+        background.getChildren().add(line_b);
+        line_a.setStartX(i * BOUND + OFFSET * 1.5);
+        line_a.setStartY(j * BOUND + OFFSET * 1.5);
+        line_a.setEndX((i + 1) * BOUND + OFFSET * 0.5);
+        line_a.setEndY((j + 1) * BOUND + OFFSET * 0.5);
+        line_a.setStroke(Color.BLUE);
+
+        line_b.setStartX((i + 1) * BOUND + OFFSET * 0.5);
+        line_b.setStartY(j * BOUND + OFFSET * 1.5);
+        line_b.setEndX(i * BOUND + OFFSET * 1.5);
+        line_b.setEndY((j + 1) * BOUND + OFFSET * 0.5);
+        line_b.setStroke(Color.BLUE);
+    }
+
+}
---
+++ src/SimpleServer.java
@@ -0,0 +1,146 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class SimpleServer implements Runnable {
+    private static final int PLAY_1 = 1; // X
+    private static final int PLAY_2 = -1; // O
+    Socket socket1;
+    Socket socket2;
+    Scanner in1;
+    PrintWriter printWriter1;
+    Scanner in2;
+    PrintWriter printWriter2;
+    int[][] chessboard;
+
+    public SimpleServer(Socket socket1, Socket socket2) throws IOException {
+        this.socket1 = socket1;
+        this.socket2 = socket2;
+    }
+
+    @Override
+    public void run() {
+        try {
+            in1 = new Scanner(socket1.getInputStream());
+            in2 = new Scanner(socket2.getInputStream());
+            printWriter1 = new PrintWriter(socket1.getOutputStream());
+            printWriter2 = new PrintWriter(socket2.getOutputStream());
+            initGame();
+        } catch (NoSuchElementException | IOException e) {
+
+        }finally {
+            System.out.println("client program unexpected exit");
+            System.out.println("close connection");
+            try {
+                socket1.close();
+                socket2.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void initGame() throws IOException {
+        try{
+            int side = 1;
+            String x;
+            String y;
+            chessboard = new int[3][3];
+            while (true) {
+                if (side == 1) {
+                    x = in1.nextLine();
+                    y = in1.nextLine();
+                    chessboard[Integer.parseInt(x)][Integer.parseInt(y)] = 1;
+                    send(printWriter2, x);
+                    send(printWriter2, y);
+                    side = - side;
+                } else {
+                    x = in2.nextLine();
+                    y = in2.nextLine();
+                    chessboard[Integer.parseInt(x)][Integer.parseInt(y)] = -1;
+                    send(printWriter1, x);
+                    send(printWriter1, y);
+                    side = - side;
+                }
+                if (isTerminate(chessboard) == PLAY_1) {
+                    send(printWriter1, "O win");
+                    send(printWriter2, "O win");
+                    break;
+                } else if (isTerminate(chessboard) == PLAY_2) {
+                    send(printWriter1, "X win");
+                    send(printWriter2, "X win");
+                    break;
+                } else if (isTerminate(chessboard) == 0) {
+                    send(printWriter1, "draw");
+                    send(printWriter2, "draw");
+                    break;
+                } else if (isTerminate(chessboard) == 505){
+                    send(printWriter1, "continue");
+                    send(printWriter2, "continue");
+                }
+                try{
+                    if(Server.start){
+                        socket1.sendUrgentData(0xFF);
+                        socket2.sendUrgentData(0xFF);
+                    }
+                }catch(Exception e){
+                    System.out.println("client program unexpected close");
+                    System.out.println("close connection");
+                    socket1.close();
+                    socket2.close();
+                    System.exit(2);
+                }
+            }
+        }catch(NoSuchElementException e){
+//            e.printStackTrace();
+        }finally{
+
+        }
+
+    }
+
+    public static void send(PrintWriter printWriter, String content){
+        printWriter.println(content);
+        printWriter.flush();
+        return;
+    }
+
+    public static int isTerminate(int[][] board){
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                if (board[i][0] == PLAY_1){
+                    return PLAY_1;
+                }else if(board[i][0] == PLAY_2){
+                    return PLAY_2;
+                }
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                if (board[0][i] == PLAY_1){
+                    return PLAY_1;
+                }else if(board[0][i] == PLAY_2){
+                    return PLAY_2;
+                }
+            }
+        }
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+                (board[0][2] == board[1][1] && board[1][1] == board[2][0])){
+            if (board[1][1] == PLAY_1){
+                return PLAY_1;
+            }else if(board[1][1] == PLAY_2){
+                return PLAY_2;
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0){
+                    return 505;
+                }
+            }
+        }
+        return 0; // draw
+    }
+}
+
+
